01a1aff082a0a1375e62b5a63bdb1caa59207a95
Shahab Uddin Ghori <154582528+Shahab-Uddin-Ghori@users.noreply.github.com>
2024-11-22 09:11:06 +0500

    pract 2 for  class x

---
+++ sum of two numbers/sumOfTwoNumbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+ cout << "\n\n To Print Sum of Two Numbers using Variables\n\n " ;
+
+ int a,b,sum;
+ cout << "\n Enter Value of a\n " ;
+ cin >> a;
+  cout << "\n Enter Value of b \n" ;
+ cin >> b;
+
+ sum = a + b;
+
+   cout << "\n\t\t======================================\n " ;
+    cout << "\t\t The Sum of a and b = " << sum;  
+   cout << " \n\t\t====================================== " ;
+
+return 0;
+}
