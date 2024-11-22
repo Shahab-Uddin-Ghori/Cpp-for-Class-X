771b9209cbbd3824c982eeffbda6a6eeb88a7c63
Shahab Uddin Ghori <154582528+Shahab-Uddin-Ghori@users.noreply.github.com>
2024-11-22 09:11:39 +0500

    pact 1 for class x

---
+++ welcome text/welcome.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //funder headers 
+
+using namespace std;
+
+int main () {
+ cout << "\n\n\t\tSir Shahab Class  ";
+ cout << "\n To print Text in new line \n ";
+ cout << "\n Welcome in Programing World class X \n ";
+}
